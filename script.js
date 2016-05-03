f14e56dd06516a2343be2f16ebb25369e9c5899b
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2016-05-03 12:35:31 +0200

    find artists added

--- script.js
+++ script.js
@@ -1,59 +1,56 @@
 $(document).ready(function(){
-	function findArtist(data){
+	
+	function findAlbums(data){
 		var url = 'http://dbpedia.org/sparql';
-		var query = ['SELECT ?result  ?artist ?albumname ?title{',
-		'?subject rdf:type <http://dbpedia.org/ontology/MusicalWork>.',
-		'?subject rdfs:label ?result.',
-		'?subject dbp:artist ?artist.',
-		'?subject dbo:album ?album.',
-		'?album dbp:thisAlbum ?albumname.',
-		'?album dbp:title ?title',
-		'FILTER(regex(?result, "^'+data+'", "i"))',
-		'}'].join(' ');
+		var query = [
+			'SELECT ?bandname ?album{',
+			'?subject rdf:type <http://dbpedia.org/ontology/Album>.',
+			'?subject rdfs:label ?album.',
+			'?subject dbo:artist ?band.',
+			'?band rdfs:label ?bandname',
+			'FILTER(regex(?bandname,"^'+data+'","i") AND lang(?bandname)="en" AND lang(?album)="en")',
+			'}'
+		].join(' ');
 		var queryUrl = encodeURI(url + '?query=' + query + '&format=json');
-		console.log("SENFSEMME");
-		$.ajax({
-		    dataType: 'jsonp',
-		    url: queryUrl,
-		    success: function(_data) {
-		    	console.log(_data);
-		        /*var results = _data.results.bindings;
-		        for (var i in results) {
-		        	console.log(results[i].artistName);
-		        	$('h1').html(results[1].artistName.value);
-		        	console.log(results[1].artistName.value);
-		        }*/
-		    }
+		$.getJSON(queryUrl,{},function(data){
+			console.log(data);
+		    var results = data.results.bindings;
+	        for (var i in results) {
+	        	$('#albumList').append("<li>"+results[i].album.value+"</li>");
+	        	//findTracks(results[i].album.value);
+	        }
 		});
+		
 	}
+
+// FIND TRACK HAUT NOCH NICHT HIN
+
+	/*function findTracks(data){
+		var url = 'http://dbpedia.org/sparql';
+		var query = [
+			'SELECT ?song ?albumname{',
+			'?subject rdf:type <http://dbpedia.org/ontology/MusicalWork>.',
+			'?subject rdfs:label ?song.',
+			'?subject dbp:fromAlbum ?album.',
+			'?album rdfs:label ?albumname.',
+			'FILTER(regex(?albumname, "^'+data+'", "i") AND lang(?albumname) = "en" AND lang(?song) = "en")',
+			'}'
+		].join(' ');
+		var queryUrl = encodeURI(url + '?query=' + query + '&format=json');
+		$.getJSON(queryUrl,{},function(data){
+			console.log(data);
+		    var results = data.results.bindings;
+	        for (var i in results) {
+	        	$('#trackList').append("<li>"+results[i].album.value+"</li>");
+	        }
+		});
+	}*/
+
 	$('#artist').change(function(){
-		findArtist($('#artist').val());
-		console.log($('#artist').val())
+		findAlbums($('#artist').val());
 	});
 });
 
-	/* GET BAND QUERY */
-	/*
-	    SELECT ?result  ?artist ?albumname ?title{
 
-		?subject rdf:type <http://dbpedia.org/ontology/MusicalWork>.
-		?subject rdfs:label ?result.
-		?subject dbp:artist ?artist.
-		?subject dbo:album ?album.
-		?subject rdfs:label ?albumname.
-		?album dbp:title ?title
 
-				
-		FILTER(regex(?result, "^Thunderstruck", "i"))
-		} 
 
-		'SELECT ?result  ?artist ?albumname ?title{',
-		'?subject rdf:type <http://dbpedia.org/ontology/MusicalWork>.',
-		'?subject rdfs:label ?result.',
-		'?subject dbp:artist ?artist.',
-		'?subject dbo:album ?album.',
-		'?album dbp:thisAlbum ?albumname.',
-		'?album dbp:title ?title',
-		'FILTER(regex(?result, "^'+data+'Thunderstruck''", "i"))',
-		'}'
-	*/
