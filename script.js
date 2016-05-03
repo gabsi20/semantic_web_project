2b08a0d86c0c63f0a96f1ba1d04fced62606fc7f
Alexander Gabriel <agabriel.mmt-b2014@fh-salzburg.ac.at>
2016-05-03 23:39:54 +0200

    added songs

--- script.js
+++ script.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
 		    var results = data.results.bindings;
 			$('#albumList').text("");
 			if(results.length == 0) {
-				column.text("No Entry found");
+				$('#memberList').text("No Entry found");
 			}
 	        for (var i in results) {
 	        	$('#albumList').append("<li>"+results[i].album.value+"</li>");
@@ -50,44 +50,50 @@ $(document).ready(function(){
 			    var results = data.results.bindings;
 				$('#memberList').text("");
 				if(results.length == 0) {
-					column.text("No Entry found");
+					$('#memberList').text("No Entry found");
 				}
 		        for (var i in results) {
 		        	console.log(results);
 		        	$('#memberList').append("<li>"+results[i].membername.value+"</li>");
 		        }
-			});
-		
+			});	
+	}
+
+	function findSongs(data){
+		var query = [
+			'Select ?song ?bandname{',
+			'?subject rdf:type <http://dbpedia.org/ontology/Song>.',
+			'?subject rdfs:label ?song.',
+			'?subject dbo:artist ?band.',
+			'?band rdfs:label ?bandname.',
+			'FILTER(regex(?bandname,"^'+data+'","i") AND lang(?song) = "en" AND lang(?bandname) = "en")',
+			'}'
+		].join(' ');
+
+		var url = 'http://dbpedia.org/sparql';
+		var queryUrl = encodeURI(url + '?query=' + query + '&format=json');
+		console.log("Query: ", query);
+		$('#trackList').text("Query is loading");
+		$.getJSON(queryUrl,{},function(data){
+			console.log("Results: ", data);
+		    var results = data.results.bindings;
+			$('#trackList').text("");
+			if(results.length == 0) {
+				$('#trackList').text("No Entry found");
+			}
+	        for (var i in results) {
+	        	console.log(results);
+	        	$('#trackList').append("<li>"+results[i].song.value+"</li>");
+	        }
+		});	
 	}
 	
 
 
-// FIND TRACK HAUT NOCH NICHT HIN
-
-	/*function findTracks(data){
-		var url = 'http://dbpedia.org/sparql';
-		var query = [
-			'SELECT ?song ?albumname{',
-			'?subject rdf:type <http://dbpedia.org/ontology/MusicalWork>.',
-			'?subject rdfs:label ?song.',
-			'?subject dbp:fromAlbum ?album.',
-			'?album rdfs:label ?albumname.',
-			'FILTER(regex(?albumname, "^'+data+'", "i") AND lang(?albumname) = "en" AND lang(?song) = "en")',
-			'}'
-		].join(' ');
-		var queryUrl = encodeURI(url + '?query=' + query + '&format=json');
-		$.getJSON(queryUrl,{},function(data){
-			console.log(data);
-		    var results = data.results.bindings;
-	        for (var i in results) {
-	        	$('#trackList').append("<li>"+results[i].album.value+"</li>");
-	        }
-		});
-	}*/
-
 	$('#artist').change(function(){
 		findAlbums($('#artist').val());
 		findMembers($('#artist').val());
+		findSongs($('#artist').val());
 	});
 });
 
